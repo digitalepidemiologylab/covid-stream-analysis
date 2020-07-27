e9c7f36e7c61b2cd735da41b578208c36b0a1bc3
Martin Müller <martin.mathias.mueller@gmail.com>
2020-07-27 13:41:59 +0200

    botometer code

--- botometer/run_botometer.py
+++ botometer/run_botometer.py
@@ -5,6 +5,9 @@ import json
 from tqdm import tqdm
 import logging
 import secrets
+import sys
+sys.path.append('..')
+from utils.helpers import get_parsed_data
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)-5.5s] [%(name)-12.12s]: %(message)s')
 logger = logging.getLogger(__name__)
@@ -19,16 +22,22 @@ bom = botometer.Botometer(wait_on_ratelimit=True, rapidapi_key=secrets.rapidapi_
 data_dir = os.path.join('..', 'data')
 annotation_data = os.path.join(data_dir, 'annotation_data', 'annotated_users_ten_languages-2.pickle')
 
-def get_usernames():
-    df = pd.read_pickle(annotation_data)
-    df = df.dropna(subset=['username'])
-    df = df.drop_duplicates(subset=['username'])
-    usernames = df.username.sample(5000)
+def get_usernames(data_type='annotation', num=5000, seed=42):
+    if data_type == 'annotation':
+        df = pd.read_pickle(annotation_data)
+        df = df.dropna(subset=['username'])
+        df = df.drop_duplicates(subset=['username'])
+        usernames = df.username.sample(num, random_state=seed)
+    elif data_type == 'raw':
+        df = get_parsed_data(usecols=['user.screen_name'])
+        df = df.dropna(subset=['user.screen_name'])
+        df = df.drop_duplicates(subset=['user.screen_name'])
+        usernames = df['user.screen_name'].sample(num, random_state=seed)
     return usernames
 
-def main():
-    usernames = get_usernames()
-    f_out = os.path.join(data_dir, 'botometer', 'botometer_results_5000.jsonl')
+def main(data_type='raw', num=5000, seed=42):
+    usernames = get_usernames(data_type=data_type, num=num, seed=seed)
+    f_out = os.path.join(data_dir, 'botometer', f'botometer_results_{num}_{data_type}.jsonl')
     for screen_name, result in tqdm(bom.check_accounts_in(usernames), total=len(usernames)):
         with open(f_out, 'a') as f:
             f.write(json.dumps(result) + '\n') 
--- botometer/sig_test_bots.py
+++ botometer/sig_test_bots.py
@@ -5,19 +5,21 @@ import sys
 sys.path.append('..')
 from utils.plot_helpers import save_fig
 import matplotlib.pyplot as plt
+import scipy.stats
 
 
 data_dir = os.path.join('..', 'data')
 annotation_data = os.path.join(data_dir, 'annotation_data', 'annotated_users_ten_languages-2.pickle')
-botometer_data = os.path.join(data_dir, 'botometer', 'botometer_results_5000.jsonl')
+botometer_data = os.path.join(data_dir, 'botometer')
 
 def get_annotation_data():
     df = pd.read_pickle(annotation_data)
     df = df.rename(columns={'username': 'user.screen_name'})
     return df
 
-def get_botometer_data():
-    df = pd.read_json(botometer_data, lines=True)
+def get_botometer_data(data_type='annotation', num=5000):
+    f_path = os.path.join(botometer_data, f'botometer_results_{num}_{data_type}.jsonl')
+    df = pd.read_json(f_path, lines=True)
     df = df.dropna(subset=['user'])
     df['user.id'] = df.user.apply(lambda s: s['id_str'])
     df['user.screen_name'] = df.user.apply(lambda s: s['screen_name'])
@@ -25,20 +27,26 @@ def get_botometer_data():
     df['cap_english'] = df.cap.apply(lambda s: s['english'])
     df['score_english'] = df.scores.apply(lambda s: s['english'])
     df['score_universal'] = df.scores.apply(lambda s: s['universal'])
+    df['is_bot'] = df['cap_universal'] > .25
     df = df.drop(columns=['user', 'cap', 'scores', 'display_scores', 'categories', 'error'])
     return df
 
 def main():
     df_annot = get_annotation_data()
-    df_bot = get_botometer_data()
+    df_bot_annot = get_botometer_data(data_type='annotation')
+    df_bot_raw = get_botometer_data(data_type='raw')
+    df = df_bot_annot.merge(df_annot, on='user.screen_name')
 
-    df = df_bot.merge(df_annot, on='user.screen_name')
-    df = df[['user.screen_name', 'cap_universal', 'n_covid_tweeting_language', 'majority_vote_category']]
-    df = df.melt(id_vars=['user.screen_name', 'n_covid_tweeting_language', 'majority_vote_category'], var_name='score', value_name='cap_score')
+    for col in ['majority_vote_category', 'majority_vote_type']:
+        num_bots = df.groupby(col).is_bot.sum()
+        num_total = df.groupby(col).is_bot.count()
+        num_tests = len(num_total)
+        mean_bot = df.is_bot.mean()
 
-    v = 2
-    sns.catplot(x="cap_score", y="n_covid_tweeting_language", kind='box', data=df, aspect=2)
-    save_fig(plt.gcf(), 'bot_distribution', v)
+        for is_bot, total, label in zip(num_bots, num_total, df.groupby(col).groups.keys()):
+            test = scipy.stats.binom_test(is_bot, total, mean_bot, alternative='greater')
+            is_sig = test < (.05/num_tests)
+            print(f'Label {label}: {test:.3f} (is_sig: {is_sig}), fraction: {is_bot/total:.3f}')
 
 if __name__ == "__main__":
     main()
